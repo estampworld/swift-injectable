04905e92bbe7f1b567f9098df6fb374fac7807d6
Eduardo Irias <eduardoirias@me.com>
2023-11-01 23:38:01 +0100

    Add Injected
    Injected
    InjectionKey
    InjectedValues

--- Package.swift
+++ Package.swift
@@ -8,16 +8,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swift-injectable",
-            targets: ["swift-injectable"]),
+            name: "Injectable",
+            targets: ["Injectable"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-injectable"),
+            name: "Injectable"),
         .testTarget(
-            name: "swift-injectableTests",
-            dependencies: ["swift-injectable"]),
+            name: "InjectableTests",
+            dependencies: ["Injectable"]),
     ]
 )
